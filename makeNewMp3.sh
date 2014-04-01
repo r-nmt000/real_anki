f449fa5dc2b4e88db753c982b88246a04d81009b
Ryusuke Nomoto <r.nmt000@gmail.com>
2014-04-01 14:29:28 +0900

    modify makeNewMp3.sh

--- makeNewMp3.sh
+++ makeNewMp3.sh
@@ -11,7 +11,7 @@ do
 	fi
     done
     if [ "${copy}" = 1 ]; then
-	cp "$i" ./alone/
+	cp "$i" ./new/
     fi
 done
 
@@ -27,64 +27,64 @@ do
 	fi
     done
     if [ "${copy}" = 1 ]; then
-	cp "$i" ./alone/
+	cp "$i" ./new/
     fi
 done
 
-for i in ./alone/*"title 0.caf"
+for i in ./new/*"title 0.caf"
 do
-    title="${i#./alone/}"
+    title="${i#./new/}"
     title="${title%-*}"
     for j in "${title}"*.caf
     do
-        cp "$j" ./alone/
+        cp "$j" ./new/
     done
 done
 
-for i in ./alone/*"t.caf"
+for i in ./new/*"t.caf"
 do
-    title="${i#./alone/}"
+    title="${i#./new/}"
     title="${title%-*}"
     for j in "${title}"*.caf
     do
-        cp "$j" ./alone/
+        cp "$j" ./new/
     done
 done
 
 
 #!/bin/sh
-find ./alone -name "A*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "B*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "C*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "D*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "E*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "F*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "G*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "H*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "I*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "J*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "K*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "L*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "M*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "N*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "O*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "P*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "Q*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "R*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "S*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "T*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "U*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "V*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "W*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "X*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "Y*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-find ./alone -name "Z*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
-for i in ./alone/*.mp4
+find ./new -name "A*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "B*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "C*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "D*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "E*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "F*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "G*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "H*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "I*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "J*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "K*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "L*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "M*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "N*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "O*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "P*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "Q*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "R*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "S*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "T*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "U*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "V*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "W*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "X*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "Y*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+find ./new -name "Z*.caf" -mtime -1 -exec afconvert -f mp4f -d aac -b 128000 {} \;
+for i in ./new/*.mp4
 do
     echo $i
     tmp=${i%.mp4}.mp3
     echo tmp
-    echo "./mp3/${tmp#./alone/}"
-    mv "$i" "./mp3/${tmp#./alone/}"
+    echo "./mp3/${tmp#./new/}"
+    mv "$i" "./mp3/${tmp#./new/}"
 
 done
